ccafa0315d5ada91f82338e1da96953db8ab8b80
Scott Albertson <ascottalbertson@gmail.com>
2019-11-08 14:38:27 -0800

    Build violation with source

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620003423) do
+ActiveRecord::Schema.define(version: 20191108222026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20180620003423) do
     t.integer "line_number"
     t.text "messages", default: [], null: false, array: true
     t.integer "file_review_id", null: false
+    t.string "source"
     t.index ["file_review_id"], name: "index_violations_on_file_review_id"
   end
 
